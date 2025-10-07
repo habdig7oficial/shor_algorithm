1db2d2ddf6a7e7de863d0bdb460d1bdb275c23a2
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2025-10-07 15:54:40 +0000

    ret

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rand::prelude::*;
+use roqoqo::{Circuit, operations:*}
 
-fn euclid(a: i32, b: i32) -> i32 {
+fn euclid(a: u32, b: u32) -> u32 {
     let mut res = a % b;
     while res != 0 {
         println!("{res}");
@@ -9,7 +10,21 @@ fn euclid(a: i32, b: i32) -> i32 {
     b
 }
 
-fn main() {
-    let n = 15 /* Taget number 3 * 5 = 15 */
+fn shor(number: u32) -> u32 {
+    let mut rng = rand::rng();
+    let base = rng.random::<u32>() % (number - 1) + 2;
+    let gdc = euclid(number, base);
 
+    println!("Number: {number}\nBase: {base}\nGDC: {gdc}\n");
+
+    if gdc == 1 {
+        return gdc;
+    }
+
+    return 0;
+}
+
+fn main() {
+    let number = 15; /* Taget number 3 * 5 = 15 */
+    shor(number);
 }
