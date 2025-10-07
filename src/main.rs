d0f84e8c5d74cbe867c6de44a509e71a6943cc35
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2025-10-07 18:37:50 +0000

    ewrfg

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,17 @@
-use rand::prelude::*;
 use roqoqo::{Circuit, operations::*};
 use roqollage;
 use roqoqo_qasm::*;
-use roqoqo_quest::Backend;
-use roqoqo::prelude::*;
 
 use std::fs::File;
 use std::io::prelude::*;
 
-fn euclid(a: u32, b: u32) -> u32 {
-    let mut res = a % b;
-    while res != 0 {
-        println!("{res}");
-        res = b % res;
-    } 
-    b
-}
+use roqoqo::prelude::EvaluatingBackend;
+use roqoqo_quest::Backend;
 
-fn shor(number: u32) -> u32 {
-    let mut rng = rand::rng();
-    let base = rng.random::<u32>() % (number - 1) + 2;
-    let gdc = euclid(number, base);
 
-    println!("Number: {number}\nBase: {base}\nGDC: {gdc}\n");
-
-    if gdc == 1 {
-        return gdc;
-    }
-
+fn main() {
     let mut circuit = Circuit::new();
-    circuit += DefinitionBit::new("$r0".to_string(), 1, true);
+    circuit += DefinitionBit::new("$r0".to_string(), 2, true);
     circuit += Hadamard::new(0);
     circuit += MeasureQubit::new(0, "$r0".to_string(), 0);
 
@@ -44,6 +26,7 @@ fn shor(number: u32) -> u32 {
     for i in asm_vec {
         openqasm += &i;
         openqasm += "\n";
+        println!("{i}")
     }
     let _ = file 
         .expect("error")
@@ -53,12 +36,6 @@ fn shor(number: u32) -> u32 {
     
     let res = simulate.run_circuit(&circuit).unwrap();
 
-    println!("{:?}",res);
+    println!("{:?}", res);
 
-    return 0;
-}
-
-fn main() {
-    let number = 15; /* Taget number 3 * 5 = 15 */
-    shor(number);
 }
