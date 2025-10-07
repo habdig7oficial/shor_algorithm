80868d5a403ba1726dbbf914bdbf966be6890f69
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2025-10-07 19:05:25 +0000

    ~fertibj

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
+use rand::prelude::*;
 use roqoqo::{Circuit, operations::*};
 use roqollage;
 use roqoqo_qasm::*;
+use roqoqo_quest::Backend;
+use roqoqo::prelude::*;
 
 use std::fs::File;
 use std::io::prelude::*;
 
-use roqoqo::prelude::EvaluatingBackend;
-use roqoqo_quest::Backend;
+fn euclid(a: u32, b: u32) -> u32 {
+    let mut res = a % b;
+    while res != 0 {
+        println!("{res}");
+        res = b % res;
+    } 
+    b
+}
 
+fn shor(number: u32) -> u32 {
+    let mut rng = rand::rng();
+    let base = rng.random::<u32>() % (number - 1) + 2;
+    let gdc = euclid(number, base);
 
-fn main() {
+    println!("Number: {number}\nBase: {base}\nGDC: {gdc}\n");
+
+    if gdc == 1 {
+        return gdc;
+    }
+
     let mut circuit = Circuit::new();
-    circuit += DefinitionBit::new("$r0".to_string(), 2, true);
+    circuit += DefinitionBit::new("$r0".to_string(), 1, true);
     circuit += Hadamard::new(0);
     circuit += MeasureQubit::new(0, "$r0".to_string(), 0);
 
@@ -26,16 +44,35 @@ fn main() {
     for i in asm_vec {
         openqasm += &i;
         openqasm += "\n";
-        println!("{i}")
     }
     let _ = file 
         .expect("error")
         .write_all(openqasm.as_bytes());
 
     let simulate = Backend::new(1, None);
-    
-    let res = simulate.run_circuit(&circuit).unwrap();
 
-    println!("{:?}", res);
+    let mut zero: i32 = 0;
+    let mut one: i32 = 0;
 
+    for i in 0..1024 {
+        let (res, _, _) = simulate.run_circuit(&circuit).unwrap();
+
+        if res["$r0"][0][0]{
+            one += 1;
+        }
+        else {
+            zero += 1;
+        }
+
+        println!("{i} - {:?}", res);
+    }
+
+    println!("Value 1 was returned: {one} times\nValue 0 was returned: {zero} times\n");
+
+    return 0;
+}
+
+fn main() {
+    let number = 15; /* Taget number 3 * 5 = 15 */
+    shor(number);
 }
